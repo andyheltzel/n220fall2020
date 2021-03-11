44327bc30cde1a3afda9ba6bafe65f197acaae67
andyheltzel <62556016+andyheltzel@users.noreply.github.com>
2021-03-11 12:01:11 -0500

    Update app.js

--- objectsHomework/dataDrivenDisplay/app/app.js
+++ objectsHomework/dataDrivenDisplay/app/app.js
@@ -1,30 +1,24 @@
 
-
 var objectProp = {
     xPos: 200,
     yPos: 200,
-    wdth: 10,
-    hght: 10,
-    color: [252, 3, 69],
+    wdth: 100,
+    hght: 100,
+    color: [54, 30, 69],
     boundaries: 400,
     create: function(){
         fill(this.color);
-        this.wdth += 1;
-        this.hght += 1;
-        noStroke();
-        rectMode(CENTER);
-        if(this.wdth === 400){
-            this.wdth = 0
-        }
-        rect(this.xPos, this.yPos, this.wdth, this.hght);
-        
+        rect(this.xPos,this.yPos, this.wdth, this.hght);
     }
 }
+
 function setup(){
     createCanvas(400,400);
 }
 
+
 function draw(){
     objectProp.create();
 }
+
 
