9251b7ab0c91c195448a0a72edd8aac4661c5a56
andyheltzel <62556016+andyheltzel@users.noreply.github.com>
2021-04-07 14:49:20 -0400

    Everything but the colors

---
+++ domHomework/thatsALottaDiv/app/app.js
@@ -0,0 +1,12 @@
+
+
+for(i = 0; i < 100; i++){
+    let squares = document.createElement("div")
+    document.body.append(squares) 
+    squares.style.width = "20px"
+    squares.style.height = "20px"
+    squares.style.float = "left"
+    squares.style.margin = "3px"
+    squares.style.backgroundColor = "#" + "ffff00"
+}
+
